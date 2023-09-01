47aa42fb492a075a0a78a43558d960c9ebd5d560
Isaiah Sanchez <isaiahsanchez.ca@gmail.com>
2023-09-01 15:31:44 -0700

    added UI for date edited and sorting

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -13,7 +13,8 @@ const HomePage = () => {
   const fetchPosts = async () => {
     try {
       const postsData = await apiService.fetchPosts();
-      const postsWithEditMode = postsData.map(post => ({ ...post, editMode: false }));
+      const sortByDate = postsData.sort((a, b) => new Date(b.lastDateEdited) - new Date(a.lastDateEdited));
+      const postsWithEditMode = sortByDate.map(post => ({ ...post, editMode: false }));
       setPosts(postsWithEditMode);
     } catch (error) {
       console.error('Error fetching posts:', error);
@@ -44,16 +45,29 @@ const HomePage = () => {
 
   const handleSave = async (id) => {
     const postToUpdate = posts.find(post => post._id === id);
-
+  
     try {
-      const updatedPost = await apiService.updatePost(id, postToUpdate);
-      setPosts(prevPosts => prevPosts.map(post => (
-        post._id === id ? { ...updatedPost, editMode: false } : post
-      )));
+      await apiService.updatePost(id, postToUpdate);
+      fetchPosts(); // Fetch all posts again after an edit to refresh the UI Not the most efficient way to do this since it adds another fetch but it works for now since the other option requires a refresh manually
     } catch (error) {
       console.error('Error updating post:', error);
     }
   };
+
+  // const handleSave = async (id) => {
+  //   const postToUpdate = posts.find(post => post._id === id);
+
+  //   try {
+  //     const updatedPost = await apiService.updatePost(id, postToUpdate);
+  //     let updatedPosts = posts.map(post => (
+  //       post._id === id ? { ...updatedPost, editMode: false } : post
+  //     ));
+  //     setPosts(updatedPosts);  // Update the state with sorted posts
+  //   } catch (error) {
+  //     console.error('Error updating post:', error);
+  //   }
+  // };
+  
 
   return (
     <Container>
@@ -99,6 +113,7 @@ const HomePage = () => {
                 <Card.Body>
                   <Card.Title>{post.exercise}</Card.Title>
                   <Card.Text>{post.equipment}</Card.Text>
+                  <Card.Text>Last Edited: {new Date(post.lastDateEdited).toLocaleString()}</Card.Text>
                   <Card.Text style={{ whiteSpace: 'pre-line' }}>{post.sets}</Card.Text>
                   <Link to={`/posts/${post._id}`}>
                     <Button variant='primary' className='mr-2'> Read More</Button>
